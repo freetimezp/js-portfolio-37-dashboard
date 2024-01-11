741385c941a13543849e59203c2e08973e8c2958
freetimezp <kraftukrnet@ukr.net>
2024-01-11 21:32:34 +0200

    08. js-portfolio-37-dashboard. Dashboard Component. Card. CardFilter.

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import './dashboard.css';
+import Card from './Card';
+
+function Dashboard() {
+    const [cards, setCards] = useState([]);
+
+    const fetchData = () => {
+        fetch('http://localhost:4000/cards')
+            .then(res => res.json())
+            .then(data => setCards(data))
+            .catch(err => console.log(err.message));
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <section className='dashboard section'>
+            <div className="row">
+                <div className="cols-lg-8">
+                    <div className="row">
+                        {cards && cards.length > 0 && cards.map(card => (
+                            <Card key={card._id} card={card} />
+                        ))}
+                    </div>
+                </div>
+                <div className="cols-lg-4"></div>
+            </div>
+        </section>
+    );
+};
+
+export default Dashboard;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './main.css';
 
 import PageTitle from './PageTitle';
+import Dashboard from './Dashboard';
 
 function Main() {
     return (
         <main id="main" className='main'>
             <PageTitle page="Dashboard" />
+            <Dashboard />
         </main>
     );
 };
