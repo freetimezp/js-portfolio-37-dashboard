82d4e5b7b357f3bfc421274175b44431e0916e9e
freetimezp <kraftukrnet@ukr.net>
2024-01-12 19:18:31 +0200

    14. js-portfolio-37-dashboard. Dashboard Component. WebTraffic. WebTrafficChart.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,6 +7,7 @@ import RecentSales from './RecentSales';
 import TopSelling from './TopSelling';
 import RecentActivity from './RecentActivity';
 import BudgetReport from './BudgetReport';
+import WebTraffic from './WebTraffic';
 
 function Dashboard() {
     const [cards, setCards] = useState([]);
@@ -44,6 +45,7 @@ function Dashboard() {
                 <div className="col-lg-4">
                     <RecentActivity />
                     <BudgetReport />
+                    <WebTraffic />
                 </div>
             </div>
         </section>
