59ac113d1cd8730d26ee4ce4b6d08f1da7fc4b01
freetimezp <kraftukrnet@ukr.net>
2024-01-12 17:04:11 +0200

    11. js-portfolio-37-dashboard. Dashboard Component. TopSelling. TopSellingItem.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,6 +4,7 @@ import './dashboard.css';
 import Card from './Card';
 import Reports from './Reports';
 import RecentSales from './RecentSales';
+import TopSelling from './TopSelling';
 
 function Dashboard() {
     const [cards, setCards] = useState([]);
@@ -32,6 +33,9 @@ function Dashboard() {
                         </div>
                         <div className="col-12">
                             <RecentSales />
+                        </div>
+                        <div className="col-12">
+                            <TopSelling />
                         </div>
                     </div>
                 </div>
---
+++ src/components/TopSellingItem.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+function TopSellingItem({ item }) {
+    return (
+        <tr>
+            <th scope="row">
+                <a href="#">
+                    <img src={item.preview} alt="top selling" />
+                </a>
+            </th>
+            <td>
+                <a href="#" className='text-primary fw-bold'>
+                    {item.name}
+                </a>
+            </td>
+            <td>
+                ${item.price.toFixed(2)}
+            </td>
+            <td className='fw-bold'>
+                {item.sold}
+            </td>
+            <td>
+                ${(item.price * item.sold).toLocaleString('en-US')}
+            </td>
+        </tr>
+    );
+};
+
+export default TopSellingItem;
