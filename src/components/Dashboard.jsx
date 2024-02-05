e2bbc6707cf4229ae81b1566484d010ecbd7e4a2
Evgen Onoprienko <51513653+freetimezp@users.noreply.github.com>
2024-02-05 12:34:12 +0200

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -16,7 +16,7 @@ function Dashboard() {
     const fetchData = () => {
         fetch('https://christmas-04.onrender.com/dashboard')
             .then(res => res.json())
-            .then(data => setCards(data[0]['cards'])
+            .then(data => setCards(data[0]['cards']))
             .catch(err => console.log(err.message));
     };
 
